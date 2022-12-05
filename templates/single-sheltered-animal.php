9e3d10775624a4feb74f9a20e181a51f48614336
alordiel <alexander.vasilev@protonmail.com>
2022-12-05 13:09:05 +0200

    added some info for the single animal template page

--- templates/single-sheltered-animal.php
+++ templates/single-sheltered-animal.php
@@ -1,6 +1,13 @@
 <?php
 get_header();
 ?>
-Single Sheltered
+<div class="shaltered-animal-container">
+	<h1><?php echo the_title(); ?></h1>
+	<div class="sinlge-animal-infobox"></div>
+	<div class="single-animal-content">
+		<?php the_content(); ?>
+	</div>
+</div>
+
 <?php
 get_footer();
