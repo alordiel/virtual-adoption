76dea42c604b77b14c03620bc2192de8d3f50072
alordiel <alexander.vasilev@protonmail.com>
2022-12-06 17:11:37 +0200

    restructuring css folders

--- assets/build/gulpfile.js
+++ assets/build/gulpfile.js
@@ -5,11 +5,11 @@ const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 
 function buildStyles() {
-  return gulp.src('./sass/**/index.scss')
+  return gulp.src('./sass/index.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css/sheltered-animals.css'));
 }
 
 exports.buildStyles = buildStyles;
