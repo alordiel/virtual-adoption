6c33596dbd3f5e00d595f4573641e2d7331a80e7
alordiel <alexander.vasilev@protonmail.com>
2023-01-06 15:58:38 +0200

    fixed some post type labels

--- include/cpt-ars-subscriptions.php
+++ include/cpt-ars-subscriptions.php
@@ -2,20 +2,20 @@
 add_action( 'init', 'ars_subscription_post_type' );
 function ars_subscription_post_type() {
 	$labels = array(
-		'name'               => _x( 'Sheltered animals', 'post type general name', 'ars-virtual-donations' ),
-		'singular_name'      => _x( 'Sheltered animal', 'post type singular name', 'ars-virtual-donations' ),
-		'menu_name'          => _x( 'Sheltered animals', 'admin menu', 'ars-virtual-donations' ),
-		'name_admin_bar'     => _x( 'Sheltered animal', 'add new on admin bar', 'ars-virtual-donations' ),
-		'add_new'            => _x( 'Add New', 'Sheltered animal', 'ars-virtual-donations' ),
-		'add_new_item'       => __( 'Add New', 'ars-virtual-donations' ),
-		'new_item'           => __( 'New sheltered animal', 'ars-virtual-donations' ),
-		'edit_item'          => __( 'Edit sheltered animal', 'ars-virtual-donations' ),
-		'view_item'          => __( 'View sheltered animal', 'ars-virtual-donations' ),
-		'all_items'          => __( 'All sheltered animals', 'ars-virtual-donations' ),
-		'search_items'       => __( 'Search sheltered animals', 'ars-virtual-donations' ),
-		'parent_item_colon'  => __( 'Parent sheltered animals:', 'ars-virtual-donations' ),
-		'not_found'          => __( 'No sheltered animal found.', 'ars-virtual-donations' ),
-		'not_found_in_trash' => __( 'No sheltered animal found in Trash.', 'ars-virtual-donations' )
+		'name'               => _x( 'Animal subscriptions', 'post type general name', 'ars-virtual-donations' ),
+		'singular_name'      => _x( 'Animal subscription', 'post type singular name', 'ars-virtual-donations' ),
+		'menu_name'          => _x( 'Animal subscriptions', 'admin menu', 'ars-virtual-donations' ),
+		'name_admin_bar'     => _x( 'Animal subscription', 'add new on admin bar', 'ars-virtual-donations' ),
+		'add_new'            => _x( 'Add New Animal subscription', 'Animal subscription', 'ars-virtual-donations' ),
+		'add_new_item'       => __( 'Add New Animal subscription', 'ars-virtual-donations' ),
+		'new_item'           => __( 'New animal subscription', 'ars-virtual-donations' ),
+		'edit_item'          => __( 'Edit animal subscription', 'ars-virtual-donations' ),
+		'view_item'          => __( 'View animal subscription', 'ars-virtual-donations' ),
+		'all_items'          => __( 'All animal subscriptions', 'ars-virtual-donations' ),
+		'search_items'       => __( 'Search animal subscriptions', 'ars-virtual-donations' ),
+		'parent_item_colon'  => __( 'Parent animal subscriptions:', 'ars-virtual-donations' ),
+		'not_found'          => __( 'No animal subscription found.', 'ars-virtual-donations' ),
+		'not_found_in_trash' => __( 'No animal subscription found in Trash.', 'ars-virtual-donations' )
 	);
 
 	$args = array(
