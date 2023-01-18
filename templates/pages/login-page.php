fbafcae9502aa7e271c2c38c971122ea267fc31d
alordiel <alexander.vasilev@protonmail.com>
2023-01-18 12:39:39 +0200

    added login page template file

---
+++ templates/pages/login-page.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template name: VirAdopt Login Page
+ */
+
+if ( is_user_logged_in() ) {
+	wp_redirect( site_url() );
+}
+get_header();
+?>
+
+	<div class="vir-adopt-login">
+		<form name="loginform" id="loginform" action="https://toafl.com/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="username" id="user_login" class="input" value="AlexVasilev"
+					   autocapitalize="none" autocomplete="username">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="ppasswordwd" id="user_pass" class="input password-input" value=""
+						   autocomplete="current-password">
+				</div>
+			</div>
+			<p class="forgetmenot"><input name="remember-me" type="checkbox" id="remember-me" value="forever"> <label
+					for="remember-me">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="login-submit" id="login-submit" class="button button-primary button-large"
+					   value="Log In">
+			</p>
+		</form>
+	</div>
+<?php
+get_footer();
+
