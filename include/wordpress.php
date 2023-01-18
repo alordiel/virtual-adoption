716a668f4d37012c4811be548fd903e02a2b3df4
alordiel <alexander.vasilev@protonmail.com>
2023-01-18 12:57:44 +0200

    added 'login-page' into the template loader

--- include/wordpress.php
+++ include/wordpress.php
@@ -1,27 +1,30 @@
 <?php
 // load templates name in page attributes
-function ars_register_template_pages( $templates ) {
-	$templates['ars-donation-checkout.php'] = 'ARS Donation Checkout page';
-	$templates['ars-thank-you-donation.php'] = 'ARS Thank you page';
-	$templates['ars-my-subscriptions.php'] = 'ARS My subscriptions page';
+function va_register_template_pages( $templates ) {
+	$templates['va-donation-checkout.php']  = 'VirtualAdoption - Donation Checkout page';
+	$templates['va-thank-you-donation.php'] = 'VirtualAdoption - Thank you page';
+	$templates['va-my-subscriptions.php']   = 'VirtualAdoption - My subscriptions page';
+	$templates['va-login-page.php']         = 'VirtualAdoption - Login page';
 
 	return $templates;
 }
 
-add_filter( 'theme_page_templates', 'ars_register_template_pages' );
+add_filter( 'theme_page_templates', 'va_register_template_pages' );
 
 // load page templates
-function ars_add_templates_pages( $template ) {
+function va_add_templates_pages( $template ) {
 	$template_slug = get_page_template_slug();
-	if ( $template_slug === 'ars-donation-checkout.php' ) {
+	if ( $template_slug === 'va-donation-checkout.php' ) {
 		$template = ARSVD_ABS . '/templates/pages/donation-checkout.php';
-	} elseif ( $template_slug === 'ars-thank-you-donation.php' ) {
+	} elseif ( $template_slug === 'va-thank-you-donation.php' ) {
 		$template = ARSVD_ABS . '/templates/pages/thank-you-donation.php';
-	} elseif ( $template_slug === 'ars-my-subscriptions.php' ) {
+	} elseif ( $template_slug === 'va-my-subscriptions.php' ) {
 		$template = ARSVD_ABS . '/templates/pages/my-subscriptions.php';
+	} elseif ( $template_slug === 'va-login-page.php' ) {
+		$template = ARSVD_ABS . '/templates/pages/login-page.php';
 	}
 
 	return $template;
 }
 
-add_filter( 'page_template', 'ars_add_templates_pages' );
+add_filter( 'page_template', 'va_add_templates_pages' );
