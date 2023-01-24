6231ca0b8df6e2935e0d6d8cd8c9907f70247ba4
Aleksandar Vasilev <alexander.vasilev@protonmail.com>
2023-01-24 16:03:45 +0200

    created separated file for the webhooks

--- include/api.php
+++ include/api.php
@@ -1,26 +1,4 @@
 <?php
-add_action( 'rest_api_init', function () {
-	register_rest_route( 'virtual-donations/v1', '/subscription/', array(
-		'methods'             => 'POST',
-		'callback'            => 'va_handle_paypal_webhook_triggered_on_subscription_change',
-		'args'                => array(
-			'id' => array(
-				'validate_callback' => function ( $param, $request, $key ) {
-					return is_numeric( $param );
-				}
-			),
-		),
-		'permission_callback' => function () {
-			return current_user_can( 'edit_others_posts' );
-		}
-	) );
-} );
-
-function va_handle_paypal_webhook_triggered_on_subscription_change() {
-
-}
-
-
 function va_create_paypal_authentication_token(): array {
 	$va_settings        = get_option( 'va-settings' );
 	$paypal_client_id   = ! empty( $va_settings['payment-methods']['paypal']['client_id'] ) ? $va_settings['payment-methods']['paypal']['client_id'] : '';
