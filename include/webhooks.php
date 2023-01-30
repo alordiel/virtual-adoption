0cdf2ac94c85fe6f4063eefbbd6ce5833467c4cd
alordiel <alexander.vasilev@protonmail.com>
2023-01-30 15:54:41 +0200

    fixes on the validation of the paypal webhook

--- include/webhooks.php
+++ include/webhooks.php
@@ -1,38 +1,65 @@
 <?php
-add_action( 'rest_api_init', function () {
+/**
+ * Registering a WP endpoint for PayPal webhook
+ *
+ * @return void
+ */
+function register_paypal_webhook() {
+
 	register_rest_route( 'virtual-donations/v1', '/subscription/', array(
 		'methods'             => 'POST',
 		'callback'            => 'va_handle_paypal_webhook_triggered_on_subscription_change',
-		'args'                => array(
-			'id' => array(
-				'validate_callback' => function ( $param, $request, $key ) {
-					$headers      = $request->get_headers();
-					$webhook_data = $request->get_json_params();
-
-					if (empty($headers['paypal_transmission_sig'][0])) {
-						return false;
-					}
-					$details   = [
-						"webhook_id"        => $webhook_data['id'][0],
-						"transmission_id"   => $headers['paypal_transmission_id'][0],
-						"transmission_time" => $headers['paypal_transmission_time'][0],
-						"cert_url"          => $headers['paypal_cert_url'][0],
-						"auth_algo"         => $headers['paypal_auth_algo'][0],
-						"transmission_sig"  => $headers['paypal_transmission_sig'][0],
-						"webhook_event"     => $request->get_params(),
-					];
-
-					$VA_paypal    = new VA_PayPal();
-					return $VA_paypal->verify_webhook_signature( $details );
-				}
-			),
-		),
-		'permission_callback' => function () {
-			return current_user_can( 'edit_others_posts' );
-		}
+		'args'                => [],
+		'permission_callback' => "__return_true",
 	) );
-} );
+}
 
-function va_handle_paypal_webhook_triggered_on_subscription_change() {
-	dbga( 'HITR' );
+add_action( 'rest_api_init', 'register_paypal_webhook' );
+
+/**
+ * Callback of the PayPal webhook
+ *
+ */
+function va_handle_paypal_webhook_triggered_on_subscription_change( WP_REST_Request $request ) {
+	if ( ! validate_paypal_request( $request ) ) {
+		return new WP_Error( '401', esc_html__( 'Not Authorized', 'virtual-adoptions' ), array( 'status' => 401 ) );
+	}
+	dbga( 'HIT' );
+	dbga( $request->get_json_params() );
+
+	return rest_ensure_response( 'This is private data.' );
+}
+
+
+function validate_paypal_request( WP_REST_Request $request ) {
+	$headers = $request->get_headers();
+	// Check if we have the needed headers
+	$headers_list = [
+		'paypal_transmission_id',
+		'paypal_transmission_time',
+		'paypal_auth_algo',
+		'paypal_cert_url',
+		'paypal_transmission_sig'
+	];
+	foreach ( $headers_list as $header ) {
+		if ( empty( $headers[ $header ][0] ) ) {
+			return false;
+		}
+	}
+	// Build the details of the data for verification
+	$va_settings = get_option( 'va-settings' );
+	$details     = [
+		"webhook_id"        => $va_settings['payment-methods']['paypal']['webhook_id'],
+		"transmission_id"   => $headers['paypal_transmission_id'][0],
+		"transmission_time" => $headers['paypal_transmission_time'][0],
+		"cert_url"          => $headers['paypal_cert_url'][0],
+		"auth_algo"         => $headers['paypal_auth_algo'][0],
+		"transmission_sig"  => $headers['paypal_transmission_sig'][0],
+		"webhook_event"     => json_encode( $request->get_params() ),
+	];
+
+	$VA_paypal = new VA_PayPal();
+
+	return $VA_paypal->verify_webhook_signature( $details );
+
 }
