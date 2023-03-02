61725c36eed2795f3686e4e161cb76423ff4a826
vagrant <vagrant@vvv>
2023-03-02 10:23:25 +0000

    added more translations and fixed some missing links

--- templates/parts/adopted-animal.php
+++ templates/parts/adopted-animal.php
@@ -4,5 +4,5 @@
  */
 ?>
 <p>
-	<?php echo sprintf( __('You have this animal in your adoption list. You can check your supported animals <a href="">here</a>.','virtual-adoptions'), $my_animals ); ?>
+	<?php echo sprintf( __('You have this animal in your adoption list. You can check your supported animals <a href="%s">here</a>.','virtual-adoptions'), $my_animals ); ?>
 </p>
